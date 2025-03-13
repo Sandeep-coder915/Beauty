95d35092229e96b13ae5d39a1d890c5f03e921cf
Sandeep kumar <er.targetsandeeepkumar@gmail.com>
2025-03-13 12:14:01 +0530

    promotional content

--- src/Components/PromotionalContent.jsx
+++ src/Components/PromotionalContent.jsx
@@ -24,14 +24,14 @@ const PromotionalContent = () => {
   }, []);
 
   return (
-    <div className={`overflow-hidden bg-white-900   py-3 flex justify-center  darkMode ? "bg-gray-900 text-white" : "bg-white text-gray-900"`}>
+    <div className="overflow-hidden bg-white-900 py-3 flex justify-center    ">
       <div
         ref={marqueeRef}
-        className="flex whitespace-nowrap space-x-10 text-lg font-semibold animate-marquee"
+        className=" text-gray-800 flex whitespace-nowrap space-x-10 text-lg font-semibold animate-marquee"
       >
         <div className="flex items-center space-x-3 hover:text-pink-400 transition">
           <Truck className="w-6 h-6 text-yellow-400" />
-          <span>Service Availible only in Amritsar</span>
+          <span className="text-gray-800">Service Availible only in Amritsar</span>
         </div>
         <div className="flex items-center space-x-3 hover:text-pink-400 transition">
           <Gift className="w-6 h-6 text-green-400" />
