def9d65d5bd6ef009b7d83f576c71644b55613cb
Sandeep kumar <er.targetsandeeepkumar@gmail.com>
2025-03-13 14:01:24 +0530

    k

--- src/Components/PromotionalContent.jsx
+++ src/Components/PromotionalContent.jsx
@@ -27,7 +27,7 @@ const PromotionalContent = () => {
     <div className="overflow-hidden bg-white-900 py-3 flex justify-center    ">
       <div
         ref={marqueeRef}
-        className=" text-gray-800 flex whitespace-nowrap space-x-10 text-lg font-semibold animate-marquee"
+        className="   flex whitespace-nowrap space-x-10 text-lg font-semibold animate-marquee"
       >
         <div className="flex items-center space-x-3 hover:text-pink-400 transition">
           <Truck className="w-6 h-6 text-yellow-400" />
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,45 +1,28 @@
 import React from 'react'
-import ServiceCard from '../Components/ServiceCard'
 import PromotionalContent from '../Components/PromotionalContent'
 import Carousel from '../Components/Carousel'
 import Services from './Services/Services'
 import WhyChooseUs from '../Components/WhyChooseUs'
-// import BeforeAfterGallery from '../Components/BeforeAfterGallery'
 import BeauticiansList from '../Components/BeauticiansList'
 import Popup from '../Components/Popup'
-// import Reviews from './Reviews'
 import TestimonialCarousel from './TestimonialCarousel'
 import TrustSection from '../Components/TrustSection'
 import FAQ from '../Components/FAQ'
-
 import ContactForm from './Contact'
-// import Popup from '../Components/Popup'
 
-const Home = ({darkMode}) => {
+const Home = ({ darkMode }) => {
   return (
     <div  >
-<Popup/>
+      <Popup />
       <Carousel />
-      {/* <TrueFocus
-sentence="Payment After Service"
-manualMode={false}
-blurAmount={5}
-borderColor="rgba(0,216,255,0.6)"
-animationDuration={0.2}
-pauseBetweenAnimations={1}
-/> */}
       <PromotionalContent className={darkMode} />
-      <TrustSection/>
+      <TrustSection />
       <Services />
-      <WhyChooseUs/>
-      <FAQ/>
-      <BeauticiansList/>
-      <ContactForm/>
-
-      {/* <BeforeAfterGallery/> */}
-{/* <Reviews/> */}
-<TestimonialCarousel/>
-
+      <WhyChooseUs />
+      <FAQ />
+      <BeauticiansList />
+      <ContactForm />
+      <TestimonialCarousel />
     </div>
   )
 }
